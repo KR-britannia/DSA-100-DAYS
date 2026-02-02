1ca916f2dc5656dc0a42da36b3284760305aeb86
KR-britannia <kr9719536297@gmail.com>
2026-02-02 21:48:01 +0530

    Add files via upload

---
+++ Day2_2.c
@@ -0,0 +1,58 @@
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.8*/
+
+#include <stdio.h>
+
+int maxProfit(int prices[], int n) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            int profit = prices[i] - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+    return maxProfit;
+}
+
+int main() {
+    int n;
+    printf("Enter number of days: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        printf("Maximum Profit: 0\n");
+        return 0;
+    }
+
+    int prices[n];
+    printf("Enter prices:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &prices[i]);
+    }
+
+    printf("Maximum Profit: %d\n", maxProfit(prices, n));
+    return 0;
+}
